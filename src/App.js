4face560dcdf4769fcb44944f4a8a9b24c11ab41
BorisBil <95983063+BorisBil@users.noreply.github.com>
2022-12-17 19:02:01 -0500

    Revert "Added Adding Employees"

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ import {
   HomePageContainer,
   TaskContainer,
   AllTasksContainer,
-  NewTaskContainer,
   EditTaskContainer,
   EmployeeContainer,
   AllEmployeesContainer,
-  EditEmployeeContainer,
-  NewEmployeeContainer
+  NewTaskContainer
 } from './components/containers';
 
 const App = () => {
@@ -22,13 +20,11 @@ const App = () => {
       <Switch>
         <Route exact path="/" component = {HomePageContainer} />
         <Route exact path="/tasks" component = {AllTasksContainer} />
-        <Route exact path="/tasks/:id" component = {TaskContainer} />
+        <Route exact path="/task/:id" component = {TaskContainer} />
         <Route exact path="/edittask/:id" component = {EditTaskContainer} />
         <Route exact path="/newtask" component = {NewTaskContainer} />
         <Route exact path="/employees" component = {AllEmployeesContainer} />
         <Route exact path="/employees/:id" component = {EmployeeContainer} />
-        <Route exact path="/editemployee/:id" component = {EditEmployeeContainer} />
-        <Route exact path="/newemployee" component = {NewEmployeeContainer} />
       </Switch>        
     </div>
   );
